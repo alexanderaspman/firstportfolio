c1a31937dba096a9a7165dd652338acccf8c0055
Alexander Aspman <aspmanalexander@gmail.com>
2020-01-02 18:01:16 +0100

    Add files via upload

---
+++ build/precache-manifest.a77b2e1991487bffb748fa54b09449b3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e0fe37f251090da120595d98d04b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77cda2da2384823cd166",
+    "url": "/static/css/main.3e24d4a7.chunk.css"
+  },
+  {
+    "revision": "e7029ce08d11ff2bf10d",
+    "url": "/static/js/2.2c5b5406.chunk.js"
+  },
+  {
+    "revision": "77cda2da2384823cd166",
+    "url": "/static/js/main.f69601a7.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "2fce3ed3f0ce58e994cb9bee43113360",
+    "url": "/static/media/Html.2fce3ed3.svg"
+  },
+  {
+    "revision": "7b70db295311e0a554164ff1dbdb2122",
+    "url": "/static/media/Vaskaorder1.7b70db29.png"
+  },
+  {
+    "revision": "8db9cd685111f6d953eb15bc5a3685fb",
+    "url": "/static/media/js.8db9cd68.svg"
+  },
+  {
+    "revision": "d9049f461eb3dd661b7f77c6e56e7593",
+    "url": "/static/media/linkedin.d9049f46.svg"
+  },
+  {
+    "revision": "0772a8c4170f6dc4b4885eee4d88c9f4",
+    "url": "/static/media/nannycutprofile.0772a8c4.jpg"
+  },
+  {
+    "revision": "be2a454d5839572e197d189910483de4",
+    "url": "/static/media/net.be2a454d.svg"
+  },
+  {
+    "revision": "8b2990918c9d4b55e39dd7997fed730e",
+    "url": "/static/media/paper-planer.8b299091.svg"
+  },
+  {
+    "revision": "996eb640641aa4f8568dcef6922ba509",
+    "url": "/static/media/phone.996eb640.svg"
+  },
+  {
+    "revision": "a4c68832d7c0cd8a8020e711c42fe981",
+    "url": "/static/media/playStore.a4c68832.jpg"
+  },
+  {
+    "revision": "7a56da88e0eacc40d4e31fb1acf2f873",
+    "url": "/static/media/profilbild.7a56da88.jpg"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/static/media/react.d8cf7740.svg"
+  },
+  {
+    "revision": "2cb1ed6b0de5926a2990bdeee09df245",
+    "url": "/static/media/ts.2cb1ed6b.png"
+  }
+]);
